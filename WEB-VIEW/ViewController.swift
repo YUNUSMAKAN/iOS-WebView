9387302d9a4021b1f9d656215fad26dde9f05666
Yunus MAKAN <makanyunus@gmail.com>
2020-09-28 12:56:26 +0300

    Initial Commit

---
+++ WEB-VIEW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WEB-VIEW
+//
+//  Created by MAKAN on 28.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
