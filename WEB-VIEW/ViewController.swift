0e483d52dccd3bb48995a5135a140fc293116afc
Yunus MAKAN <makanyunus@gmail.com>
2020-11-02 22:17:28 +0300

    initial commit!

--- WEB-VIEW/ViewController.swift
+++ WEB-VIEW/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let html = ""
+        let htmlPath = Bundle.main.path(forResource: "sample", ofType: "html")
+        let url = URL(fileURLWithPath: htmlPath!)
+        webView.load(URLRequest(url: URL(string: "http://51.124.79.0/api/?do=event/detail&id=17")!))
     }
 
-
+    
 }
 
